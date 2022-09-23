6838596b49bfafac9f4605e080788b7cb86f1a63
Mick van Duijn <m.vanduijn@zefiros.io>
2022-09-23 11:57:48 +0200

    feat: initial implementation

---
+++ sqs.tf
@@ -0,0 +1,98 @@
+locals {
+  is_fifo = try(var.fifo_settings.enabled, false)
+  has_dlq = try(var.dlq_settings.enabled, false)
+}
+
+resource "aws_sqs_queue" "this" {
+  name                       = local.is_fifo
+  name_prefix                = var.name_prefix
+  visibility_timeout_seconds = var.visibility_timeout_seconds
+  max_message_size           = var.max_message_size
+  delay_seconds              = var.delay_seconds
+  receive_wait_time_seconds  = var.receive_wait_time_seconds
+  policy                     = var.policy
+  tags                       = var.tags
+
+  # Using the SQS account-bound key is secure enough
+  #tfsec:ignore:aws-sqs-queue-encryption-use-cmk
+  kms_master_key_id                 = var.kms_master_key_id
+  kms_data_key_reuse_period_seconds = var.kms_data_key_reuse_period_seconds
+
+  # FIFO settings
+  fifo_queue                  = local.is_fifo
+  fifo_throughput_limit       = local.is_fifo ? var.fifo_settings.throughput_limit : null
+  content_based_deduplication = local.is_fifo ? var.fifo_settings.content_based_deduplication : null
+  deduplication_scope         = local.is_fifo ? var.fifo_settings.deduplication_scope : null
+
+  lifecycle {
+    precondition {
+      condition     = var.name != null || var.name_prefix != null
+      error_message = "Either name or name_prefix must be provided"
+    }
+
+    precondition {
+      condition     = var.name == null || var.name_prefix == null
+      error_message = "Either name or name_prefix must be provided, not both"
+    }
+
+    precondition {
+      condition     = var.kms_master_key_id != null
+      error_message = "Queue must be encrypted using KMS"
+    }
+  }
+}
+
+# DLQ settings
+resource "aws_sqs_queue_redrive_policy" "this" {
+  count     = local.has_dlq ? 1 : 0
+  queue_url = aws_sqs_queue.this.url
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.dlq[count.index].arn
+    maxReceiveCount     = var.dlq_settings.max_receive_count
+  })
+}
+resource "aws_sqs_queue_redrive_allow_policy" "this" {
+  queue_url = aws_sqs_queue.this.url
+  # Don't allow the primary queue to redrive into another queue
+  redrive_allow_policy = jsonencode({
+    redrivePermission = "denyAll"
+  })
+}
+
+
+resource "aws_sqs_queue" "dlq" {
+  count = local.has_dlq ? 1 : 0
+
+  name                       = var.name != null ? "${var.name}${var.dlq_settings.suffix}" : null
+  name_prefix                = var.name_prefix != null ? "${var.name_prefix}${var.dlq_settings.suffix}" : null
+  visibility_timeout_seconds = var.dlq_settings.visibility_timeout_seconds
+  max_message_size           = var.max_message_size
+  delay_seconds              = var.dlq_settings.delay_seconds
+  receive_wait_time_seconds  = var.dlq_settings.receive_wait_time_seconds
+  policy                     = var.dlq_settings.policy != null ? var.dlq_settings.policy : var.policy
+  tags                       = var.tags
+
+  # Using the SQS account-bound key is secure enough
+  #tfsec:ignore:aws-sqs-queue-encryption-use-cmk
+  kms_master_key_id                 = var.kms_master_key_id
+  kms_data_key_reuse_period_seconds = var.kms_data_key_reuse_period_seconds
+
+  # FIFO settings
+  fifo_queue                  = local.is_fifo
+  fifo_throughput_limit       = local.is_fifo ? var.fifo_settings.throughput_limit : null
+  content_based_deduplication = local.is_fifo ? var.fifo_settings.content_based_deduplication : null
+  deduplication_scope         = local.is_fifo ? var.fifo_settings.deduplication_scope : null
+}
+resource "aws_sqs_queue_redrive_allow_policy" "dlq" {
+  count     = local.has_dlq ? 1 : 0
+  queue_url = aws_sqs_queue.this.url
+
+  # Redrive only works for non-fifo queues
+  redrive_allow_policy = var.dlq_settings.redrive_enabled && !local.is_fifo ? jsonencode({
+    redrivePermission = "byQueue",
+    sourceQueueArns   = [aws_sqs_queue.this.arn]
+    }) : jsonencode({
+    redrivePermission = "denyAll"
+  })
+}
+
---
+++ variables.tf
@@ -0,0 +1,87 @@
+variable "name" {
+  description = "Name of the queue (cannot be combined with name_prefix)."
+  type        = string
+  default     = null
+}
+
+variable "name_prefix" {
+  description = "Name prefix for the queue (cannot be combined with name)."
+  type        = string
+  default     = null
+}
+
+variable "visibility_timeout_seconds" {
+  description = "Delay before messages received (using ReceiveMessage, e.g. Lambda subscription) become visible again."
+  type        = number
+  default     = 30
+}
+
+variable "max_message_size" {
+  description = "Maximum message size (don't provide this, unless you have a real use-case to restrict this)."
+  type        = number
+  default     = null
+}
+
+variable "delay_seconds" {
+  description = "Delay before messages sent into the queue become visible."
+  type        = number
+  default     = null
+}
+
+variable "receive_wait_time_seconds" {
+  description = "Wait time before returning empty receives. This can be used to configure queue-level long polling."
+  type        = number
+  default     = null
+}
+
+variable "policy" {
+  description = "Resource policy for the SQS queue."
+  type        = string
+}
+
+variable "kms_master_key_id" {
+  description = "KMS key to encrypt messages with."
+  type        = string
+  default     = "alias/aws/sqs"
+}
+
+variable "kms_data_key_reuse_period_seconds" {
+  description = "Length of time for SQS to reuse the KMS data key. Higher settings will reduce the amount of KMS API calls."
+  type        = number
+  default     = null
+}
+
+variable "tags" {
+  description = "Tags to put on the queue (besides the provider level default_tags)."
+  type        = map(string)
+  default     = null
+}
+
+variable "fifo_settings" {
+  description = "Settings to setup this queue as a FIFO queue. If enabled, the queue is by default set up as a high-throughput FIFO."
+  type = object({
+    enabled                     = bool
+    content_based_deduplication = optional(bool, true)
+    deduplication_scope         = optional(string, "messageGroup")
+    throughput_limit            = optional(string, "perMessageGroup")
+  })
+  default = null
+}
+
+variable "dlq_settings" {
+  description = "Settings related to the DLQ. If the queue is configured as FIFO, then redrive from the AWS Console will NOT be enabled."
+  type = object({
+    enabled           = bool
+    suffix            = optional(string, "-dlq")
+    max_receive_count = optional(number, 5)
+    redrive_enabled   = optional(bool, true)
+
+    visibility_timeout_seconds = optional(number)
+    delay_seconds              = optional(number)
+    receive_wait_time_seconds  = optional(number)
+    policy                     = optional(string)
+  })
+  default = {
+    enabled = true
+  }
+}
